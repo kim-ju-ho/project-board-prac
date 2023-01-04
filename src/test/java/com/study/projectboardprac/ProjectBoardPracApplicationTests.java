a70c0e4ad5800868c3314d98cedd61ee5d69d961
kim-ju-ho <kim1jo2ho3@naver.com>
2023-01-04 20:46:25 +0900

    #6-data

---
+++ src/test/java/com/study/projectboardprac/ProjectBoardPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.projectboardprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoardPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
